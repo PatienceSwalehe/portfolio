0eb5be85be744ea454f796699865d1240ae99fbf
SMITH EXAUCE YAYAMA <exauceyayama@gmail.com>
2025-07-28 12:58:25 +0000

    update

---
+++ script/script.js
@@ -0,0 +1,12 @@
+ const bouton = document.getElementById('btn-menu');
+const menu = document.getElementById('navbar-cache');
+const FermerMenu = document.getElementById('navbar-clicable');
+
+bouton.addEventListener('click', function() {
+    menu.style.display = 'flex';
+    bouton.style.display = 'none';
+});
+FermerMenu.addEventListener('click', function() {
+    menu.style.display = 'none';
+    bouton.style.display = 'block';
+});
